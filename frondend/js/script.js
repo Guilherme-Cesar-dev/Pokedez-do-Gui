02b1740ff3daa09095618c3345647fc4e3445d8c
Guilherme-Cesar-dev <162651959+Guilherme-Cesar-dev@users.noreply.github.com>
2025-07-15 00:26:30 -0300

    Sistema de cartas pelo js e json

---
+++ frondend/js/script.js
@@ -0,0 +1,51 @@
+let pokemons = [];
+let currentIndex = 0;
+const pageSize = 8;
+
+function renderPokemons() {
+  const container = document.getElementById('pokemon-list');
+  let html = '';
+  const nextPokemons = pokemons.slice(currentIndex, currentIndex + pageSize);
+  nextPokemons.forEach(pokemon => {
+    html += `
+      <div class="card">
+        <div class="container">
+          <div class="img-pokemon">
+            <img src="${pokemon.imagem}" alt="${pokemon.nome}">
+          </div>
+          <div class="pokemon">
+            <h1 id="number">N° ${pokemon.id}</h1>
+            <div class="informacao">
+              <h3 id="name">${pokemon.nome}</h3>
+              ${pokemon.tipos.map(tipo => `<h4 class="type" id="${tipo}">${tipo}</h4>`).join('')}
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+  container.innerHTML += html;
+  currentIndex += pageSize;
+
+  // Verifica se chegou ao fim
+  if (currentIndex >= pokemons.length) {
+    document.getElementById('load-more').style.display = 'none';
+    const endMsg = document.createElement('p');
+    endMsg.textContent = 'Não existem mais Pokemons =( ';
+    endMsg.style.textAlign = 'center';
+    endMsg.style.fontWeight = 'bold';
+    endMsg.style.color = '#a4a4a4';
+    container.appendChild(endMsg);
+  }
+}
+
+fetch('/backend/pokemons.json')
+  .then(response => response.json())
+  .then(data => {
+    pokemons = data;
+    currentIndex = 0;
+    document.getElementById('pokemon-list').innerHTML = '';
+    renderPokemons();
+  });
+
+document.getElementById('load-more').addEventListener('click', renderPokemons);
